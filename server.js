0680c31af8198d8a3729e3599cf60731b1478a2f
psforza <psforza@redhat.com>
2016-10-10 19:22:29 +0200

    a

--- server.js
+++ server.js
@@ -23,12 +23,11 @@ wsServer = new WebSocketServer({
 
 //Handle Countries
 var countries = fs.readFileSync("countriesMinimal.json")
-  function findCountryCode(country) { 
+function findCountryCode(country) { 
     return country.alpha2 == "ZW";
-  };
-console.log((new Date()) + ' Countries file loaded');
-
-console.log((new Date()) + ' Find Res.: ' + countries.find(findCountryCode));
+}
+console.log((new Date()) + ' Countries file loaded')
+console.log((new Date()) + ' Find Res.: ' + countries.find(findCountryCode))
 console.log((new Date()) + ' Index Res.: ' + countries.indexOf("ZW"));
 
 var inventory = [
