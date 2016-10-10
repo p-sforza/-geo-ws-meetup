fc89c4144f91ff8bda198ee067b65eb9ddf2c5e2
psforza <psforza@redhat.com>
2016-10-10 23:37:56 +0200

    a

--- server.js
+++ server.js
@@ -78,10 +78,27 @@ wsServer.on('request', function(request) {
 
 //Web Socket Client to meetup API
   var api_url = "ws://stream.meetup.com:80/2/rsvps/";
-  var ws = new WebSocketClient(api_url, 'echo-protocol');
-  ws.addEventListener("message", function(e) {
-	  var msg = e.data;
-	  console.log(msg);
+  var client = new WebSocketClient();
+  
+  client.on('connectFailed', function(error) {
+      console.log('Connect Error: ' + error.toString());
   });
-
+   
+  client.on('connect', function(connection) {
+      console.log('WebSocket Client Connected');
+      connection.on('error', function(error) {
+          console.log("Connection Error: " + error.toString());
+      });
+      connection.on('close', function() {
+          console.log('echo-protocol Connection Closed');
+      });
+      connection.on('message', function(message) {
+          if (message.type === 'utf8') {
+              console.log("Received: '" + message.utf8Data + "'");
+          }
+      });
+      
+  });
+   
+  client.connect(api_url, 'echo-protocol');
 
